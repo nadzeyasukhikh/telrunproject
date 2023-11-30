355a24a517cfff264c19367ba972e4efb67fe8b4
nadzeya <vitserresvit@gmail.com>
2023-11-30 19:06:19 +0200

    not found

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,6 +1,22 @@
+import { useNavigate } from "react-router-dom"
+import notFound from "../../assets/images/404.svg"
+import styles from "./NotFound.module.css"
+
+
+
+
 function NotFound(){
+    const navigateNotFound = useNavigate()
+
+
     return(
-        <>not found</>
+        <div className={styles.notFoundDiv}>
+           <img src={notFound} alt="not found" />
+           <h3>Page Not Found</h3>
+           <p>We’re sorry, the page you requested could not be found.
+Please go back to the homepage.</p>
+<button onClick={() => navigateNotFound("/")}>Go Home</button>
+        </div>
     )
 }
 
