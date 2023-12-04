84a554435756807d4b6cf267a13928f6626ce5ee
nadzeya <vitserresvit@gmail.com>
2023-12-04 13:58:48 +0200

    scroll

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import NotFound from './pages/notFound/NotFound';
 
 
 
+
 function App() {
   
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
-
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
   <BrowserRouter>
+  <ScrollToTop />
     <App />
     </BrowserRouter>
     </Provider>
