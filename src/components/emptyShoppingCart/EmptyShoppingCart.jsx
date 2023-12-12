1820df08cf9da4ec4e757f24535e2a77d33679fd
nadzeya <vitserresvit@gmail.com>
2023-12-12 18:22:49 +0200

    empty shopping cart

--- src/components/emptyShoppingCart/EmptyShoppingCart.jsx
+++ src/components/emptyShoppingCart/EmptyShoppingCart.jsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./EmptyShoppingCart.module.css"
+
 function EmptyShoppingCart(){
+
+    const navigate = useNavigate();
+    const navigateToShop = useNavigate()
     return (
-        <div>
-            <div>
-            <h2>Shopping cart</h2>
-            <button>Back to the store</button>
+        <div className={styles.emptyDiv}>
+            <div className={styles.titleBtn}>
+            <h2 className={styles.emptyTitle}>Shopping cart</h2>
+            <div className={styles.titleBtnBar}></div>
+            <button className={styles.storeBtn} onClick={() => navigate("/")}>Back to the store</button>
             </div>
-            <p>Looks like you have no items in your basket currently.</p>
-            <button>Continue Shopping</button>
+            <p className={styles.emptyText}>Looks like you have no items in your basket currently.</p>
+            <button className={styles.emptyBtn} onClick={() => navigateToShop("/allProducts")}>Continue Shopping</button>
         </div>
     )
 }
